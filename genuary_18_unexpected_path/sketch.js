03fae606ecbf0f7dff5a9dbe381df41482678363
megamitts <megamitts@gmail.com>
2026-01-18 10:20:35 +0000

    Add sketch.js for random walk based on pi digits

---
+++ genuary_18_unexpected_path/sketch.js
@@ -0,0 +1,59 @@
+let piDigits;
+let walker;
+let index = 0;
+
+function preload() {
+  rand = random(17892348449839565896214465623318536478, 314159265358979323846264338327950288419716939937510);
+  piDigits = rand.toString();
+}
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+  walker = createVector(width / 2, height / 2);
+  //stroke(255);
+  //strokeWeight(2);
+}
+
+function draw() {
+  
+  
+  
+  // Get current digit
+  stroke(random(50,200));
+  let digit = int(piDigits.charAt(index));
+  strokeWeight(random(digit));
+  
+  // Map digit to movement direction
+  // 0-1: up, 2-3: right, 4-5: down, 6-7: left, 8-9: random diagonal
+  let step = random(5);
+  if (digit <= 1) {
+    walker.y -= step;
+  } else if (digit <= 3) {
+    walker.x += step;
+  } else if (digit <= 5) {
+    walker.y += step;
+  } else if (digit <= 7) {
+    walker.x -= step;
+  } else {
+    walker.x += random([-step, step]);
+    walker.y += random([-step, step]);
+  }
+
+  // Draw the step
+  point(walker.x, walker.y);
+
+  // Wrap around edges
+  if (walker.x < 0) walker.x = width;
+  if (walker.x > width) walker.x = 0;
+  if (walker.y < 0) walker.y = height;
+  if (walker.y > height) walker.y = 0;
+
+  // Move to next digit
+  index++;
+  if (index >= piDigits.length) {
+    index = 0; //loop
+  }
+  
+  
+}
