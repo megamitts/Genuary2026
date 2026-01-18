b9dda5eb9e950eae6804187e4552751208249a5f
megamitts <megamitts@gmail.com>
2026-01-18 17:19:40 +0000

    Refactor walker to use object instead of vector

--- genuary_18_unexpected_path/sketch.js
+++ genuary_18_unexpected_path/sketch.js
@@ -10,7 +10,13 @@ function preload() {
 function setup() {
   createCanvas(400, 400);
   background(0);
-  walker = createVector(width / 2, height / 2);
+  walker = {
+    
+    x: width/2,
+    y: height/2
+    
+  };
+  //walker = createVector(width / 2, height / 2);
   //stroke(255);
   //strokeWeight(2);
 }
